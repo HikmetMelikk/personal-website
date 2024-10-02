6d88649085beb1b89997664d9ea1e8d4ca4f697f
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-10-02 14:10:49 +0300

    Deleted unnecessary splitStringUsingRegex utility function

--- src/utils/splitStringUsingRegex.tsx
+++
@@ -1,9 +0,0 @@
-export default function splitStringUsingRegex(inputString: string): string[] {
-    const characters: string[] = [];
-    const regex = /[\s\S]/gu;
-    let match;
-    while ((match = regex.exec(inputString)) !== null) {
-        characters.push(match[0]);
-    }
-    return characters;
-}
