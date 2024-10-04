ba78da6bc37f062fc801e6ee9600e6428b0a1f3e
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-10-04 22:17:04 +0300

    Refactor MyWorks component and update styles
    - Refactored the MyWorks component to improve code readability and maintainability.
    - Updated the styles in myworks.module.css to add a responsive grid layout for MyWorksCard components on larger screens.

--- src/components/MyWorks.tsx
+++ src/components/MyWorks.tsx
@@ -3,13 +3,15 @@ import styles from "../styles/myworks.module.css";
 import MyWorksCard from "./MyWorksCard";
 
 export default function MyWorks() {
-    const [lang] = useTranslation("global");
+	const [lang] = useTranslation("global");
 
-    return (
-        <div className={styles.myWorksSection} id="my-works">
-            <h1>{lang("myWorks.title")}</h1>
-            <p>{lang("myWorks.description")}</p>
-            <MyWorksCard />
-        </div>
-    );
+	return (
+		<div className={styles.myWorksSection} id="my-works">
+			<h1>{lang("myWorks.title")}</h1>
+			<p>{lang("myWorks.description")}</p>
+			<div className={styles.myWorksCardSection}>
+				<MyWorksCard />
+			</div>
+		</div>
+	);
 }
